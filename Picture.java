2056dc0b114831d7614b0930c4b23281a3fba36e
Montse <mtschamorro@gmail.com>
2014-10-15 09:01:22 +0200

    crear metodo puesta de sol y blanco y negro

---
+++ Picture.java
@@ -0,0 +1,110 @@
+/**
+ * This class represents a simple picture. You can draw the picture using
+ * the draw method. But wait, there's more: being an electronic picture, it
+ * can be changed. You can set it to black-and-white display and back to
+ * colors (only after it's been drawn, of course).
+ *
+ * This class was written as an early example for teaching Java with BlueJ.
+ * 
+ * @author  Michael Kšlling and David J. Barnes
+ * @version 2011.07.31
+ */
+public class Picture
+{
+    private Square wall;
+    private Square window;
+    private Triangle roof;
+    private Circle suelo1;
+    private Circle sun;
+    
+    /**
+     * Constructor for objects of class Picture
+     */
+    public Picture()
+    {
+        // nothing to do... instance variables are automatically set to null
+    }
+
+    /**
+     * Draw this picture.
+     */
+    public void draw()
+    {
+        wall = new Square();
+        wall.moveHorizontal(-140);
+        wall.moveVertical(20);
+        wall.changeSize(120);
+        wall.makeVisible();
+        
+        window = new Square();
+        window.changeColor("black");
+        window.moveHorizontal(-120);
+        window.moveVertical(40);
+        window.changeSize(40);
+        window.makeVisible();
+
+        roof = new Triangle();  
+        roof.changeSize(60, 180);
+        roof.moveHorizontal(20);
+        roof.moveVertical(-60);
+        roof.makeVisible();
+
+        sun = new Circle();
+        sun.changeColor("blue");
+        sun.moveHorizontal(100);
+        sun.moveVertical(-40);
+        sun.changeSize(80);
+        sun.makeVisible();
+        
+        suelo1 = new Circle();
+        suelo1.changeColor("green");
+        suelo1.moveHorizontal(-990);
+        suelo1.moveVertical(165);
+        suelo1.changeSize(2000);
+        suelo1.makeVisible();
+        
+        suelo1.makeVisible();
+        
+    }
+    
+    /**
+     * Move Sol
+     */
+    public void moveSun()
+    {sun.slowMoveVertical(150);
+     suelo1.makeVisible();
+     wall.changeColor("black");
+     window.changeColor("white");
+     roof.changeColor("black");
+     suelo1.changeColor("black");
+     sun.changeColor("black");
+    }
+        
+        /**
+     * Change this picture to black/white display
+     */
+    public void setBlackAndWhite()
+    {
+        if (wall != null)   // only if it's painted already...
+        {
+            wall.changeColor("black");
+            window.changeColor("white");
+            roof.changeColor("black");
+            sun.changeColor("black");
+        }
+    }
+
+    /**
+     * Change this picture to use color display
+     */
+    public void setColor()
+    {
+        if (wall != null)   // only if it's painted already...
+        {
+            wall.changeColor("red");
+            window.changeColor("black");
+            roof.changeColor("green");
+            sun.changeColor("yellow");
+        }
+    }
+}
